d49abeb124d40fc19d667e7af03aca1e8919cff1
Maninder Singh <manindersingh120996@gmail.com>
2023-11-04 19:32:14 +0530

    Built upto Multi Head Attention Layer

--- Complete-Transformer from scratch/model.py
+++ Complete-Transformer from scratch/model.py
@@ -130,20 +130,86 @@ class FeedForwardBlock(nn.Module):
         """
         Input sentence of shape :
         
-        (Batch, Seq_Len, d_model)
+        ( Batch, Seq_Len, d_model )
                     |
                     |
-        (Applying Linear Layer )
-                    |
-                    |        
-        (Batch, Seq_Len, d_ff)
+        ( Applying Linear Layer )
                     |
                     |        
-        (Applying Linear Layer )
+        ( Batch, Seq_Len, d_ff )
                     |
                     |        
-        (Batch, Seq_Len, d_model) 
+        ( Applying Linear Layer )
+                    |
+                    |        
+        ( Batch, Seq_Len, d_model ) 
         """
         return self.linear_2(self.droput(torch.relu(self.linear_1(x))))
+
+# Most Important Block - Building Multihead Attention
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, heads: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.heads = heads
+        assert d_model % heads == 0, "d_model is not divisible by h"
+        
+        self.d_k = d_model // heads
+
+        #defining weight parameter metrics
+        self.w_q = nn.Linear(d_model, d_model) # query metrics
+        self.w_k = nn.Linear(d_model, d_model) # weight metrics
+        self.w_v = nn.Linear(d_model, d_model) # value metrics
+
+        self.w_o = nn.Linear(d_model, d_model) # Ouput weight metrics, used after heads concatination
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value,mask,dropout:nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (batch, h , Seq_len, d_k) --> (batch, h , Seq_Len, Seq_Len)
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) #(Batch, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        """
+        mask is there to hide some words so that they do not 
+        interact other words(usefull in algorithms like BERT), so it will guess those words
+        compoare the output with actual values and adjust the weights accordingly
+
+        Also, we will be spliting query,key and value into number of heads, and while
+        splitting: 
+        we will be keeping 'batch_dimension' i.e., query.shape[0] UNCHANGED
+        we will keep 'Seq_len' i.e., query.shape[1] UNCHANGED
+
+        only, spliting 'd_model' into (Heads x d_k)
+        """
+        query = self.w_q(q)  # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        key = self.w_k(k)   # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        value = self.w_v(v)  # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+
+        # (Batch, Seq_len, d_model) --> (Batch, Seq_len,heads, d_k) --> (Batch, heads, Seq_len, d_k) 
+
+        query = query.view(query.shape[0], query.shape[1], self.heads, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.heads, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.heads, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+
+        # (Batch, heads, seq_len, d_k) --> (batch, seq_len, heads, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1, 2).contguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # (Batch, Seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+
 
 
