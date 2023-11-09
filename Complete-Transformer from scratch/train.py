f361cd8094e277d2523fd8f74dbcc243c96ffe72
Maninder Singh <manindersingh120996@gmail.com>
2023-11-09 14:40:39 +0530

    code built upto Tokenizer in Train.py file

--- Complete-Transformer from scratch/train.py
+++ Complete-Transformer from scratch/train.py
@@ -13,6 +13,16 @@ from tokenizers.pre_tokenizers import Whitespace
 
 from pathlib import Path
 
+
+def get_all_sentences(ds, lang):
+    """
+    getting all the sentences to prepare dataset of the particular language
+
+    """
+
+    for item in ds:
+        yield item['translation'][lang]
+
 def get_or_build_tokenizer(config, ds, lang):
     """
     This Tokenizer code is taken from hugging face library
@@ -25,4 +35,14 @@ def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
     if not Path.exists(tokenizer_path):
         tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        # pre tokenizer stand that we are spliting the tokens depending upon white space
+        tokenizer.pre_tokenizer = Whitespace()
+        #building traineer to train our tokenizer
+        trainer  = WordLevelTrainer(special_tokens = ["[UNK]","[PAD]", "[SOS]", "[EOS]"],
+                                min_frequency = 2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer = trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
 
