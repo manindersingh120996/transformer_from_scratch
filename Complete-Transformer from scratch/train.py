2cb6b967aca9590652f14677697741308be0b778
Maninder Singh <manindersingh120996@gmail.com>
2023-11-09 12:14:01 +0530

    Tokenizer builiding in between

--- Complete-Transformer from scratch/train.py
+++ Complete-Transformer from scratch/train.py
@@ -11,3 +11,18 @@ from tokenizers.models import WordLevel
 from tokenizers.trainers import  WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
+from pathlib import Path
+
+def get_or_build_tokenizer(config, ds, lang):
+    """
+    This Tokenizer code is taken from hugging face library
+
+    config : input configuiration for our model
+    ds : dataset to build tokenizer on
+    lang : the language of the dataset
+    """
+    # path of tokenizer file and it depends upon the input language
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+
