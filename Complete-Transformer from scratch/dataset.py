ccdacfd251dbcfe5ce9aa6746cc71b4494112460
Maninder Singh <manindersingh120996@gmail.com>
2023-11-10 12:41:24 +0530

    built upto return statement in dataset file __getitem__

--- Complete-Transformer from scratch/dataset.py
+++ Complete-Transformer from scratch/dataset.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-member
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
@@ -28,3 +29,57 @@ class BilingualDataset(Dataset):
 
         # converting extracted text into tokens and coverting it into ids
         enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+
+        # reason for -2 : since apart from sentence we will also be adding EOS and SOS token 
+        # in the sentence thus -2 
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2
+        # here will be adding only 1 special token that is SOS token
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
+
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError('Sentence is too long')
+
+        # adding SOS and EOS to the source text
+
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+        
+        # adding SOS to the decoder input
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        # add EOS to the label (what we expect as output from the decoder)
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ]
+        )
+
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
+
+        return{
+            "encoder_input":encoder_input, # (seq_len),
+            "decoder_input":decoder_input, # (seq_len)
+            # since we are increasing the len of the input tokens by adding PAD tokens
+            # but we don't want to include them in training and ignore them, thus encoder_mask
+            "encoder_mask" : (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), #(1,1, Seq_Len)
+            # for DECODER we need SPECIAL MASK, that is each word can only look previous words and non padding words only
+            "decoder_mask" : (decoder_input != self.pad_token).unsqueeze(0).unsqeeze(0).int() & causal_mask(decoder_input.size(0)) # (1, Seq_len) & (1, Seq_len, Seq_len)
+        }
+
