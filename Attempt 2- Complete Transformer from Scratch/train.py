f6968ba49ec51438cd0d2f610c43a55f876d6038
Maninder Singh <manindersingh120996@gmail.com>
2025-05-16 18:36:12 +0530

    training loop completed

--- Attempt 2- Complete Transformer from Scratch/train.py
+++ Attempt 2- Complete Transformer from Scratch/train.py
@@ -8,8 +8,11 @@ from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 from dataset import BilingualDataset
 from model import build_transformer
-
+from torch.utils.tensorboard import SummaryWriter
+from config import get_config, get_weights_file_path
+from tqdm import tqdm
 from pathlib import Path
+
 
 def get_all_sentences(ds,lang):
     for item in ds:
@@ -67,3 +70,85 @@ def get_model(config,
               vocab_tgt_len,):
     model = build_transformer(vocab_src_len,vocab_tgt_len,config['seq_len'], config['seq_len'], config['d_model'])
     return model
+
+def train_model(config):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device : {device}")
+    
+    Path(config['model_folder']).mkdir(parents=True, exist_ok = True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
+
+    model = get_model(config, tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size()).to(device)
+
+    # writing to tensor board
+    writer = SummaryWriter(config['experiment_name'])
+
+    optimizer = torch.optim.Adam(model.parameters(), lr = config['lr'], eps=1e-9)
+
+    initial_epoch = 0
+    global_step = 0
+    if config['preload']:
+        model_filename = get_weights_file_path(config, config['preload'])
+        print(f'Preloading model {model_filename}')
+        state = torch.load(model_filename)
+        initial_epoch = state['epoch'] + 1
+        optimizer.load_state_dict(state['optimizer_state_dict'])
+        global_step = state['global_step']
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
+
+    for epoch in range(initial_epoch,config['num_epochs']):
+        model.train()
+        batch_iterator = tqdm(train_dataloader, desc=f"Processing epoch {epoch:02d}")
+        for batch in batch_iterator:
+            encoder_input = batch['encoder_input'].to(device) # (B, Seq_Len)
+            decoder_input = batch['decoder_input'].to(device) # (B, Seq_Len)
+            # encoder mask to mask padding tokens
+            encoder_mask = batch['encoder_mask'].to(device) #(B,1,1 Seq_len)
+            decoder_mask = batch['decoder_mask'].to(device) # (B, 1, Seq_len, Seq_len)
+
+            # passing the sentences through the transformer model
+            encoder_output = model.encode(encoder_input, encoder_mask) # B, seq_len, d_model
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # b, seq_len, d_model
+            
+            # converting to get the output token reppresentation
+            proj_output = model.project(decoder_output)
+
+            label = batch['label'].to(device) # B, seq_len
+
+            # b, seq_len, tgt_vocab_size -> b* seq_len, tgt_vocab_size
+            # labels with raw score and not one hot encoded
+            # projecting the output to the target vocab size and then calculating the loss
+            loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+            batch_iterator.set_postfix({f"loss :" : f"{loss.item():6.3f}"})
+
+            # logging the loss to tensorboard
+            writer.add_scalar('traininig loss' , loss.item(), global_step)
+            writer.flush()
+
+            # loss backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        # saving the model at the end of each epoch
+        model_filename = get_weights_file_path(config, f'{epoch:02d}')
+        torch.save({
+            'epoch' : epoch,
+            'model_state_dict' : model.state_dict(),
+            'optimizer_state_dict' : optimizer.state_dict(),
+            'global_step': global_step
+        }, model_filename)
+
+
+if __name__ == '__main__':
+    config = get_config()
+    train_model(config)
+
+
+
+
+
+
+
